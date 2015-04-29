1ffbcc5bd0de87e5c7dfc5481c902a6efc07350b
AndraDenis <andra_denis1@yahoo.com>
2015-04-29 19:23:24 +0300

    My first commit

---
+++ AndraDenis.java
@@ -0,0 +1,10 @@
+public class AndraDenis {
+	public static void main (String[] args) {
+		int[] array = {1, 2, 3, 4};
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		System.out.println(sum);
+	}
+}
