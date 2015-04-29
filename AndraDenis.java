5b08430031c387e8695952250373f5a7898dbd63
AndraDenis <andra_denis1@yahoo.com>
2015-04-29 19:43:46 +0300

    2nd commit

--- AndraDenis.java
+++ AndraDenis.java
@@ -2,9 +2,11 @@ public class AndraDenis {
 	public static void main (String[] args) {
 		int[] array = {1, 2, 3, 4};
 		int sum = 0;
+		double ave = 0.0;
 		for (int i = 0; i < array.length; i++) {
 			sum += array[i];
 		}
-		System.out.println(sum);
+		ave = sum / array.length;
+		System.out.println(sum + " " + ave);
 	}
 }
