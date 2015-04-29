de95091ee1cf22e9a7da04eb168bbbaa9f64f16d
IoanaTeodorescu <teodorescu_ioana94@yahoo.com>
2015-04-29 19:23:31 +0300

    My first commit

---
+++ Clasa.java
@@ -0,0 +1,28 @@
+
+
+public class Clasa{
+
+public static void main(String[] args){
+
+	int x=7;
+	int y=5;
+	int max=0;
+	if(x>y)
+		max=x;
+	else 
+		max=y;
+
+	System.out.println("Maximul este: "+max);
+
+
+
+}
+
+
+
+
+
+
+
+
+}
